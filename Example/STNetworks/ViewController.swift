a2aae3a213140537e86cecbce8d502a97105417a
zhangxs <zhangxs@startimes.com.cn>
2017-11-23 17:29:34 +0800

    @feature，STNetworks0.1.0

--- Example/STNetworks/ViewController.swift
+++ Example/STNetworks/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import STNetworks
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let name = "这是一个测试私有库引用私有specs仓库的工程"
+        print(name.addSuffix())
+        print(name.st_addSuffix())
     }
 
     override func didReceiveMemoryWarning() {
---
+++ STNetworks/Classes/STString+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  STNetworks
+//
+//  Created by 张晓珊 on 2017/11/23.
+//
+
+import Foundation
+import Networks
+
+extension String {
+    public func st_addSuffix() -> String {
+        return self.addSuffix() + "——STNetworks(version:0.1.0)"
+    }
+}
